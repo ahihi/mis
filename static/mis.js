0ce8296e3a21fe6e45b276683a50f31739e190a1
Miranda Kastemaa <miranda@foldplop.com>
2017-08-17 04:11:28 +0300

    Reconnect when WebSocket closes

--- static/mis.js
+++ static/mis.js
@@ -2,6 +2,17 @@ var MAP_CENTER = [60.1699, 24.9384];
 var MAP_ZOOM = 13.5;
 
 window.onload = function() {
+    var map_div = document.querySelector("#map");
+    
+    function set_map(on) {
+	if(on) {
+	    map_div.style.display = "block";
+	    map.invalidateSize();
+	} else {
+	    map_div.style.display = "none";
+	}
+    };
+
     var map = L.map("map")
 	.setView(MAP_CENTER, MAP_ZOOM);
 
@@ -17,24 +28,39 @@ window.onload = function() {
     var ws_url = ws_protocol + "//" + loc.host + "/ws";
 
     var circle = undefined;
-    var ws = new WebSocket(ws_url);
 
-    ws.onmessage = function(ev) {
-	var msg = JSON.parse(ev.data);
-	console.log(msg);
+    function open_websocket() {
+	var ws = new WebSocket(ws_url);
 
-	if(circle === undefined) {
-	    circle = L.circle([msg.latitude, msg.longitude], {
-		color: "#FF0000",
-		fillColor: "#FF0033",
-		fillOpacity: 0.5,
-		radius: msg.accuracy
-	    }).addTo(map);
-	} else {
-	    circle.setRadius(msg.accuracy);
-	    circle.slideTo([msg.latitude, msg.longitude], {
-		duration: 500
-	    });
+	ws.onopen = function() {
+	    set_map(true);
+	};
+
+	ws.onclose = function() {
+	    set_map(false);
+	    setTimeout(open_websocket, 2000);
 	}
-    };
+	
+	ws.onmessage = function(ev) {
+	    var msg = JSON.parse(ev.data);
+
+	    if(circle === undefined) {
+		circle = L.circle([msg.latitude, msg.longitude], {
+		    color: "#FF0000",
+		    fillColor: "#FF0033",
+		    fillOpacity: 0.5,
+		    radius: msg.accuracy
+		}).addTo(map);
+	    } else {
+		circle.setRadius(msg.accuracy);
+		circle.slideTo([msg.latitude, msg.longitude], {
+		    duration: 500
+		});
+	    }
+	};
+    }
+
+    set_map(false);
+    
+    open_websocket();
 };
